f68325d5f88a233fec9ba2c2026611d47613c362
Michael Klos <michael.klos@gmail.com>
2025-12-15 22:52:36 -0500

    Run collectstatic in entrypoint to fix 500 error on missing manifest

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Applying database migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting Gunicorn..."
 exec gunicorn feverish.wsgi:application --bind 0.0.0.0:8000
